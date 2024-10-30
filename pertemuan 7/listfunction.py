8eab7c2e42f384a2174e160c1d620417d9f22b48
devanooo <trestantodevano@gmail.com>
2024-10-30 17:09:53 +0700

    Praktikum 7

--- pertemuan 7/listfunction.py
+++ pertemuan 7/listfunction.py
@@ -21,9 +21,67 @@ def konsi(L, e):
 # +=========================================================================+ #
 def Isempty(L):
     return L == []
+        
+def isoneelemet(L):
+    if Isempty(L):
+        return False
+    else:
+        return Tail(L) == [] and Head(L) == []
+# +=========================================================================+ #
+#                           REALISASI SELEKTOR
+# +=========================================================================+ #
+def FirstElmnt(L):
+    if Isempty(L):
+        return None
+    else : 
+        return L[0]
+def LastElmnt(L):
+    if Isempty(L):
+        return None
+    else :return L[-1]
+def Tail(L):
+    if Isempty(L):
+        return []
+    else : return L[1:]
+def Head(L):
+    if Isempty(L):
+        return []
+    else : return L[:-1]
+# +=========================================================================+ #
+#                           REALISASI OPERATOR
+# +=========================================================================+ #
+# NbElmnt: List -> Integer
+def NbElmnt(L):
+    if Isempty(L):
+        return 0
+    else : 
+        return 1 + NbElmnt(Tail(L))
 # +=========================================================================+ #
 #                                Aplikasi
 # +=========================================================================+ #
 
-print("Hasil Konso:", konso(1,[2,3,4,5]))
-print("Hasil Konso:", konsi([1,2,3,4],5))
+# print("Hasil Konso:", konso(1,[2,3,4,5]))
+# print("Hasil Konso:", konsi([1,2,3,4],5))
+
+# print("\nHasil IsEmpty :", Isempty([]))
+# print("Hasil IsEmpty :", Isempty([1,2,3,4,5]))
+# print("Hasil IsOneElement :", isoneelemet([]))
+# print("Hasil IsOneElement :", isoneelemet([1]))
+# print("Hasil IsOneElement :", isoneelemet([1,2,3,4,5]))
+
+# print("\nHasil FirstElement :",FirstElmnt([])) 
+# print("Hasil FirstElement :",FirstElmnt([1,2,3,4,5])) # - > 1
+# print("\nHasil LastElement :",LastElmnt([])) 
+# print("Hasil LastElement :",LastElmnt([1,2,3,4,5])) # - > 5
+
+# print("\nHasil Tail: ",Tail([0]))
+# print("Hasil Tail: ",Tail([1,2,3,4,5]))
+# print("Hasil Head: ",Head([0]))
+# print("Hasil Head: ",Head([1,2,3,4,5]))
+
+# print("\nHasil NbElmnt: ",NbElmnt([]))
+# print("Hasil NbElmnt: ",NbElmnt([0]))
+# print("Hasil NbElmnt: ",NbElmnt([1,1,1,1,1]))
+# print("Hasil NbElmnt: ",NbElmnt([1,2,3,4,5]))
+
+
---
+++ pertemuan 7/nyobain.py
@@ -0,0 +1,3 @@
+import listfunction as lst
+
+print(lst.isoneelemet([1]))
